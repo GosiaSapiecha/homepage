b0f56aba39e58722f56c8d4eb4b9e90117c75e7d
Gosia Sapiecha <malgosiasapiecha@gmail.com>
2022-11-11 21:40:25 +0100

    centered nav in header

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const name = 'Gosia';
-const age = 15;
+// const name = 'Gosia';
+// const age = 15;
 
-console.log(`czesc nazywam sie ${name} i mam ${age}`);
-alert(`czesc nazywan sie ${name} i mam ${age}`);
+// console.log(`czesc nazywam sie ${name} i mam ${age}`);
+// alert(`czesc nazywan sie ${name} i mam ${age}`);
