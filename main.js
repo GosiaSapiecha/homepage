f928ece2a1f83c383049d13123cfb4504a248147
Gosia Sapiecha <malgosiasapiecha@gmail.com>
2022-12-03 16:53:07 +0100

    added function in JS

--- main.js
+++ main.js
@@ -5,8 +5,7 @@
     `cześć nazywam sie ${firstName} i mam ${age}`
     );
 
-
-const header = document.querySelector('.main-header--js');
+    const header = document.querySelector('.main-header--js');
 
 header.innerHTML = `cześć nazywan sie ${firstName} i mam ${age}`
 
@@ -15,3 +14,18 @@ console.log(header);
 const emptyParagraph = document.querySelector('.paragraph__second--js');
 
 emptyParagraph.innerHTML = `Tekst napisany JavaScriptem`;
+
+function greet(age, firstName) {
+    console.log(
+        `cześć nazywam sie ${firstName} i mam ${age}`
+        );
+}
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+createContent('.paragraph__second--js', 'Witaj świecie'); 
+
+createContent('.heading--js', 'Podmieniony tytuł'); 
